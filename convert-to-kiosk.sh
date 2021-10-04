5f7e22d96c711f6d6d2b1d86acd73dcde7b18eae
TJ Biegner <tbiegner99@gmail.com>
2021-10-04 15:17:26 -0400

    script typo

--- convert-to-kiosk.sh
+++ convert-to-kiosk.sh
@@ -8,11 +8,12 @@ echo "xset s noblank        # turn off screen blanking" >> /etc/xdg/openbox/auto
 echo "xset s off            # turn off screen saver" >> /etc/xdg/openbox/autostart
 echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' ~/.config/chromium/'Local State'" >> /etc/xdg/openbox/autostart
 echo "sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/; s/\"exit_type\":\"[^\"]\+\"/\"exit_type\":\"Normal\"/' ~/.config/chromium/Default/Preferences" >> /etc/xdg/openbox/autostart
-echo "chromium-browser  --noerrdialogs --check-for-update-interval=31536000 --disable-background-networking --disable-component-update --disable-infobars --kiosk $KIOSK_URL"
+echo "chromium-browser  --noerrdialogs --check-for-update-interval=31536000 --disable-background-networking --disable-component-update --disable-infobars --kiosk $KIOSK_URL"  >> /etc/xdg/openbox/autostart
 cat  /etc/xdg/openbox/autostart
 
 echo "export KIOSK_URL=http://127.0.0.1/thermostat">>/etc/xdg/openbox/environment
 
 touch /home/pi/.bash_profile
 echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && startx -- -nocursor' >> /home/pi/.bash_profile
-source /home/pi/.bash_profile && reboot
+source /home/pi/.bash_profile 
+reboot
