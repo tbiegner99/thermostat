27e70f6a3fd312df3680d74c5b96e3b4265f89d5
TJ Biegner <tbiegner99@gmail.com>
2024-12-05 20:11:57 -0500

    update to 5 min

--- setup-monitor.sh
+++ setup-monitor.sh
@@ -3,6 +3,6 @@ PWD=`pwd`
 
 sudo rm -f /etc/cron.d/monitor-thermostat
 sudo touch /etc/cron.d/monitor-thermostat
-sudo echo "*/1 * * * * root $PWD/check-health.sh $PWD >> /var/log/monitor-thermostat.log 2>&1" > /etc/cron.d/monitor-thermostat
+sudo echo "*/5 * * * * root $PWD/check-health.sh $PWD >> /var/log/monitor-thermostat.log 2>&1" > /etc/cron.d/monitor-thermostat
 sudo chmod 600 /etc/cron.d/monitor-thermostat
 echo "Chron set up"
