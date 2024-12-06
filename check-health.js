561035abd86b6ee14cff4ccf3712d7972edfe0bb
TJ Biegner <tbiegner99@gmail.com>
2024-12-05 19:41:53 -0500

    health check logging

--- check-health.js
+++ check-health.js
@@ -3,21 +3,27 @@ async function checkHealth() {
     const healthCheck = await fetch("http://localhost/api/health");
     //fail if the health check fails
     if (healthCheck.status !== 200) {
-      console.log("Health check failed with status " + healthCheck.status);
+      console.log(
+        `${new Date().toISOString()} - Health check failed with status ${
+          healthCheck.status
+        }`
+      );
       process.exit(-1);
     }
     //fail if last temperature collection is older than 5 minutes
     const responseBody = await healthCheck.json();
 
-    const now = new Date().valueOf();
+    const now = new Date();
     const lastUpdate = Date.parse(responseBody.lastUpdate);
-    if (now - lastUpdate > 5 * 60 * 1000) {
-      console.log("Last update is older than 5 minutes");
+    if (now.valueOf() - lastUpdate > 5 * 60 * 1000) {
+      console.log(
+        `${now.toISOString()} - Health check failed. Update is older than 5 minutes`
+      );
       process.exit(-3);
     }
-    console.log("Health check passed");
+    console.log(`${now.toISOString()} - Health check passed`);
   } catch (error) {
-    console.log("Health check failed");
+    console.log(`${now.toISOString()} - Health check failed`);
     process.exit(-2);
   }
   process.exit(0);
--- setup-monitor.sh
+++ setup-monitor.sh
@@ -3,6 +3,6 @@ PWD=`pwd`
 
 sudo rm -f /etc/cron.d/monitor-thermostat
 sudo touch /etc/cron.d/monitor-thermostat
-sudo echo "*/1 * * * * root $PWD/check-health.sh" > /etc/cron.d/monitor-thermostat
+sudo echo "*/1 * * * * root $PWD/check-health.sh >> /var/log/monitor-thermostat.log" > /etc/cron.d/monitor-thermostat
 sudo chmod 600 /etc/cron.d/monitor-thermostat
 echo "Chron set up"
