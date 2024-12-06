75aaaa85161512f9cf94be5049031b6bca230769
TJ Biegner <tbiegner99@gmail.com>
2024-12-05 19:26:56 -0500

    add

--- setup-monitor.sh
+++ setup-monitor.sh
@@ -5,3 +5,4 @@ sudo rm -f /etc/cron.d/monitor-thermostat
 sudo touch /etc/cron.d/monitor-thermostat
 sudo echo "*/1 * * * * root $PWD/check-health.sh" > /etc/cron.d/monitor-thermostat
 sudo chmod 600 /etc/cron.d/monitor-thermostat
+echo "Chron set up"
